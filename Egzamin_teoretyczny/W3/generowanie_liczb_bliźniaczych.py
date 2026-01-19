2be7d59347a006267d7984fd2af9d8f3554e1348
Kinga Jutrzenka Trzebiatowska <jutrzenkatkinga@gmail.com>
2026-01-19 16:44:37 +0300

    Add files via upload

---
+++ Egzamin_teoretyczny/W3/generowanie_liczb_bliźniaczych.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+def naiwny_test_pierwszości(n):
+	i = 2
+	while i * i <= n:
+		if n%i == 0:
+			return False
+		else:
+			i += 1
+	return True
+
+zakres = int(input("Podaj zakres genrowania liczb bliźniaczych "))
+
+for k in range(2, zakres-1):
+	if naiwny_test_pierwszości(k) and naiwny_test_pierwszości(k+2) :
+			print(f"Liczby bliźniacze {k}, {k+2}")
