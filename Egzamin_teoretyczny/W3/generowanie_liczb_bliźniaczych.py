bd3b6b551fed4fb3183cc8e8fe89974134953a73
Kinga Jutrzenka Trzebiatowska <jutrzenkatkinga@gmail.com>
2026-01-19 15:46:50 +0100

    Remove unnecessary comments and clean up code

--- Egzamin_teoretyczny/W3/generowanie_liczb_bliźniaczych.py
+++ Egzamin_teoretyczny/W3/generowanie_liczb_bliźniaczych.py
@@ -15,21 +15,3 @@ for k in range(2, zakres-1):
 	if naiwny_test_pierwszości(k) and naiwny_test_pierwszości(k+2) :
 			print(f"Liczby bliźniacze {k}, {k+2}")
 
-#Faktoryzacja zgodnie z algorytmem
-
-def Faktoryzacja(n, lista): #podajemy liczbę n którą będziemy rozkładać na czynniki pierwsze oraz listę liczb pierwszych
-	i = 0  #indeks listy liczb pierwszych
-	czynniki = [] #lista z czynnikami które powstaną podczas rozkładu
-	while n > 1: #działamy póki n>1
-		if n%lista[i] == 0: #jeśli reszta z dzielenia przez liczbę o danym indeksie w liście liczb pierwszych == 0
-			czynniki=czynniki + [lista[i]] #dodajemy nasz czynnik do listy
-			n=n//lista[i] #dzielimy naszą liczbę n przez czynn
-		else:
-			i=i+1 #zwiększamy i, by móc brać kolejne liczby pierwsze
-	return(czynniki)
-
-pierwsze = [2, 3, 5, 7, 11, 13, 17, 19, 23]
-czynniki=Faktoryzacja(int(input("Podaj liczbę: ")), pierwsze)
-print("Oto czynniki pierwsze XD: ")
-for i in czynniki:
-	print(i)
